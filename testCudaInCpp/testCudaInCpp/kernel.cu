694e1ae087c0c4aeeada14d8aa324ab3bd01d3f0
wangzi6147 <363177773@qq.com>
2014-10-20 16:00:19 +0800

    修改现存内存交互

--- testCudaInCpp/testCudaInCpp/kernel.cu
+++ testCudaInCpp/testCudaInCpp/kernel.cu
@@ -10,6 +10,13 @@ void addWithCuda(int *c, int *b, int *a, int size){
 	int *dev_a = 0;
 	int *dev_b = 0;
 	int *dev_c = 0;
-
+	cudaSetDevice(0);
+	cudaMalloc((void**)&dev_c, size * sizeof(int));
+	cudaMalloc((void**)&dev_a, size * sizeof(int));
+	cudaMalloc((void**)&dev_b, size * sizeof(int));
+	cudaMemcpy(dev_c, c, size * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
 	addKernel << <1, size >> >(dev_c, dev_a, dev_b);
+	cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
 }
